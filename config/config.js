50f71bfa51cb45569139c46ba118c395d8931691
J. Smith <joe.a.smith@gmail.com>
2019-12-16 20:47:39 -0600

    trying to resolve an issue with git bash

---
+++ config/config.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const key = require("./key");
+
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": key.SQLPASS,
+    "database": "devdb",
+    "host": "localhost",
+    "dialect": "mysql"
+  },
+  test: {
+    "username": "root",
+    "password": key.SQLPASS,
+    "database": "devdb",
+    "host": "localhost",
+    "dialect": "mysql",
+    "logging": false
+  },
+  production: {
+    "use_env_variable": "JAWSDB_URL",
+    "dialect": "mysql"
+  }
+}
