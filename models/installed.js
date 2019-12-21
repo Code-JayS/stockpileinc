acc3b69a1a38d0b59f565741d89c0ceb74a308cd
J. Smith <joe.a.smith@gmail.com>
2019-12-21 12:39:12 -0600

    resolved api routes and sequelize db creation errors

--- models/installed.js
+++ models/installed.js
@@ -1,20 +1,25 @@
-module.exports = function(sequelize, DataTypes) {
-  var Installed = sequelize.define("Installed", {
-    installedLocation: {
-      type: DataTypes.STRING,
-      allowNul: false,
-      validate: {
-        len: [1]
+module.exports = function (sequelize, DataTypes) {
+  var Installed = sequelize.define(
+    "Installed",
+    {
+      installedLocation: {
+        type: DataTypes.STRING,
+        allowNul: false,
+        validate: {
+          len: [1]
+        }
+      },
+      serialNumber: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+          len: [1]
+        }
       }
     },
-    serialNumber: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-      validate: {
-        len: [1]
-      }
-    },
-    freezeTableName: true
-  });
+    {
+      freezeTableName: true
+    }
+  );
   return Installed;
 };
