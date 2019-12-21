cab5080a9269686c91f42edb6d14a59269c8516a
Robert Davidson <davidsonrobert1996@gmail.com>
2019-12-21 11:38:20 -0600

    added login page and registration page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -12,14 +12,14 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/login", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
       res.render("example", {
-        example: dbExample
+        msg: "Welcome!",
+        examples: dbExamples
       });
     });
   });
-
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
