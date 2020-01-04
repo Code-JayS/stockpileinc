425fe23ec100d072be6678de376ea7f8ff80f837
Robert Davidson <davidsonrobert1996@gmail.com>
2020-01-03 19:25:01 -0600

    added parts page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -30,6 +30,14 @@ module.exports = function(app) {
       });
     });
   });
+  app.get("/parts", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("parts", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+  });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
