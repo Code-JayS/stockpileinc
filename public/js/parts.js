2dcda36b3599b29dff35820354a33ea448397cdd
J. Smith <joe.a.smith@gmail.com>
2020-01-05 22:25:58 -0600

    parts.js file reworked

---
+++ public/js/parts.js
@@ -0,0 +1,36 @@
+$(document).ready(() => {
+  $(document).on("submit", ".formNewPart", handleNewPart);
+
+  function handleNewPart(event) {
+    event.preventDefault();
+
+    var newPart = {
+      name: $("#inputPart")
+        .val()
+        .trim(),
+      vendorName: $("#inputVendor")
+        .val()
+        .trim(),
+      type: $("#inputType")
+        .val()
+        .trim(),
+      price: $("#inputPrice")
+        .val()
+        .trim(),
+      stock: $("#inputStock")
+        .val()
+        .trim(),
+      description: $("#inputDescription")
+        .val()
+        .trim(),
+      picture: $("#inputPicture")
+        .val()
+        .trim()
+    };
+    console.log(newPart);
+
+    $.post("/api/newPart", newPart).then(function(res) {
+      res.end();
+    });
+  }
+});
